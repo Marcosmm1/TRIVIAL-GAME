8a056b5e2a57c5b629202b1fcc93116c172431e5
Ollebacx <dav.ollebac@gmail.com>
2020-04-13 12:37:21 +0100

    Home page design

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <h1>Trivia Quizzy</h1>
+    <p>Select your difficulty:</p>
+    <v-btn color="success" outlined x-large v-model="difficulty">easy</v-btn>
+    <v-btn color="warning" outlined x-large v-model="difficulty">medium</v-btn>
+    <v-btn color="error" outlined x-large v-model="difficulty">hard</v-btn>
+
+    <v-btn color="success" x-large bloc>Start</v-btn>
+
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ApiService from "../services/TrivialService.js";
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  difficulty: "",
+  create() {
+    ApiService.getQuestions(this.difficulty).then(response =>(this.questions = response))
   }
-}
+};
 </script>
+
+<style lang="scss" scoped>
+.home{
+  text-align:center;
+}
+</style>
