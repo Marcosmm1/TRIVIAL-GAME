09154a3866ac23eb96bfebb299e34aedaa3a07a9
Ollebacx <dav.ollebac@gmail.com>
2020-04-13 12:49:53 +0100

    Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,26 @@
 <template>
   <div class="home">
+<v-container>
 
-    <h1>Trivia Quizzy</h1>
+<h1>Trivia Quizzy</h1>
     <p>Select your difficulty:</p>
-    <v-btn color="success" outlined x-large v-model="difficulty">easy</v-btn>
-    <v-btn color="warning" outlined x-large v-model="difficulty">medium</v-btn>
-    <v-btn color="error" outlined x-large v-model="difficulty">hard</v-btn>
+    <div>
+    <v-btn class="button" color="success" outlined x-large v-model="difficulty">easy</v-btn>
 
-    <v-btn color="success" x-large bloc>Start</v-btn>
+    </div>
+        <div>
+    <v-btn class="button" color="warning" outlined x-large v-model="difficulty">medium</v-btn>
+
+    </div>
+        <div>
+    <v-btn class="button" color="error" outlined x-large v-model="difficulty">hard</v-btn>
+
+    </div>
+
+    <v-btn color="success" x-large block>Start</v-btn>
+
+</v-container>
+
 
 
   </div>
@@ -28,5 +41,10 @@ export default {
 <style lang="scss" scoped>
 .home{
   text-align:center;
+  max-width: 70%;
 }
+ .button {
+    width: 150px;
+    margin: 8px;
+  }
 </style>
